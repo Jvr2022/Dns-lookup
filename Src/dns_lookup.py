a9eacd83d6cf091084185cb77553240c1a7db311
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-11 21:23:15 +0200

    Update dns_lookup.py

--- Src/dns_lookup.py
+++ Src/dns_lookup.py
@@ -1,13 +1,21 @@
 import socket
 
-def perform_dns_lookup(domain):
+def view_ip_addresses(domain):
     try:
-        ip_address = socket.gethostbyname(domain)
-        return ip_address
+        ip_addresses = socket.getaddrinfo(domain, None)
+        ipv4_set = set(ip[4][0] for ip in ip_addresses if ip[1] == socket.AF_INET)
+        ipv6_set = set(ip[4][0] for ip in ip_addresses if ip[1] == socket.AF_INET6)
+        
+        ip_set = ipv4_set.union(ipv6_set)
+        return list(ip_set)
     except socket.gaierror:
-        return "DNS lookup failed for {}".format(domain)
+        return ["DNS lookup failed for {}".format(domain)]
 
 if __name__ == "__main__":
     domain_name = input("Enter the domain name: ")
-    result = perform_dns_lookup(domain_name)
-    print("IP address for {} is: {}".format(domain_name, result))
+    result = view_ip_addresses(domain_name)
+    
+    if result[0].startswith("DNS lookup failed"):
+        print(result[0])
+    else:
+        print("IP addresses for {} are: {}".format(domain_name, ', '.join(result)))
