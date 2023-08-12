a2a6f7cd1e56ccce9249edb3806efff57006fcac
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-12 09:07:37 +0200

    Geolocation of IP addresses.

--- Src/dns_lookup.py
+++ Src/dns_lookup.py
@@ -1,6 +1,9 @@
 import socket
+import requests
+import dns.resolver
 
-DNS_SERVER = "8.8.8.8"
+DNS_SERVERS = ["8.8.8.8", "8.8.4.4"]
+IPINFO_API_URL = "http://ipinfo.io/{}/json"
 
 def view_ip_addresses(domain):
     try:
@@ -15,34 +18,54 @@ def view_ip_addresses(domain):
 
 def retrieve_mx_records(domain):
     try:
+        resolver = dns.resolver.Resolver(configure=False)
+        resolver.nameservers = DNS_SERVERS
+        
         mx_records = []
-        answers = socket.getaddrinfo(domain, None, socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        answers = resolver.resolve(domain, 'MX')
         
         for answer in answers:
-            if answer[1] == socket.SOCK_STREAM:
-                mx_records.append(answer[4][0])
+            mx_records.append(answer.exchange.to_text())
         
         return mx_records
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+        return ["No MX records found for {}".format(domain)]
+
+def get_ip_geolocation(ip_address):
+    try:
+        response = requests.get(IPINFO_API_URL.format(ip_address))
+        data = response.json()
+        return data.get("city", "Unknown city"), data.get("region", "Unknown region"), data.get("country", "Unknown country")
+    except requests.RequestException:
+        return "Failed to fetch geolocation"
+
+def fast_dns_query(domain):
+    try:
+        dns_query = socket.gethostbyname(domain)
+        return dns_query
     except socket.gaierror:
-        return ["DNS lookup failed for {}".format(domain)]
+        return "DNS query failed for {}".format(domain)
 
 if __name__ == "__main__":
     domain_name = input("Enter the domain name: ")
     ip_result = view_ip_addresses(domain_name)
     mx_result = retrieve_mx_records(domain_name)
-    
-    if ip_result[0].startswith("DNS lookup failed"):
-        print(ip_result[0])
-    else:
-        print("IP addresses for {} are: {}".format(domain_name, ', '.join(ip_result)))
 
-    if mx_result[0].startswith("DNS lookup failed"):
-        print(mx_result[0])
-    else:
-        print("MX records for {} are: {}".format(domain_name, ', '.join(mx_result)))
+if ip_result[0].startswith("DNS lookup failed"):
+    print(ip_result[0])
+else:
+    print("IP addresses for {} are:".format(domain_name))
+    for ip in ip_result:
+        city, region, country = get_ip_geolocation(ip)
+        print("- IP address {}: {}, {}, {}".format(ip, city, region, country))
 
-    try:
-        dns_query = socket.gethostbyname(domain_name)
-        print("Fast and reliable DNS query for {} is: {}".format(domain_name, dns_query))
-    except socket.gaierror:
-        print("DNS query failed for {}".format(domain_name))
+if mx_result[0].startswith("No MX records"):
+    print(mx_result[0])
+else:
+    print("\nMX records for {} are:".format(domain_name))
+    for mx_server in mx_result:
+        print("- {}".format(mx_server))
+
+dns_query_result = fast_dns_query(domain_name)
+print("\nFast and reliable DNS query for {} is:".format(domain_name))
+print("- {}".format(dns_query_result))
