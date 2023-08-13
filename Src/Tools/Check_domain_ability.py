b0d2a8391382f15d15f8e409d212e5f56609b813
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-13 20:00:09 +0200

    Update Check_domain_ability.py

--- Src/Tools/Check_domain_ability.py
+++ Src/Tools/Check_domain_ability.py
@@ -14,3 +14,18 @@ if __name__ == "__main__":
     domain_name = input("Enter the domain name: ")
     result = check_domain_availability(domain_name)
     print(result)
+
+def ask_to_restart():
+    while True:
+        choice = input("Do you want to start again? (y/n): ").lower()
+        if choice == 'y':
+            return True
+        elif choice == 'n':
+            return False
+        else:
+            print("Invalid choice. Please enter 'y' for yes or 'n' for no.")
+
+if ask_to_restart():
+    pass
+else:
+    print("Program closed.")
