b4f3b6d54e181da6532f1e2d1ae89e74245d11fb
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-14 09:19:49 +0200

    Update MX_record.py

--- Src/Tools/MX_record.py
+++ Src/Tools/MX_record.py
@@ -14,31 +14,27 @@ def retrieve_mx_records(domain):
             mx_records.append(answer.exchange.to_text())
         
         return mx_records
-    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+    except dns.resolver.NXDOMAIN:
+        return ["Domain {} not found.".format(domain)]
+    except dns.resolver.NoAnswer:
         return ["No MX records found for {}".format(domain)]
+    except dns.exception.DNSException as e:
+        return ["Error retrieving MX records: {}".format(e)]
 
 if __name__ == "__main__":
-    domain_name = input("Enter the domain name: ")
-    mx_result = retrieve_mx_records(domain_name)
-    
-    if mx_result[0].startswith("No MX records"):
-        print(mx_result[0])
-    else:
-        print("MX records for {} are:".format(domain_name))
-        for mx_server in mx_result:
-            print("- {}".format(mx_server))
-
-def ask_to_restart():
     while True:
-        choice = input("Do you want to start again? (y/n): ").lower()
-        if choice == 'y':
-            return True
-        elif choice == 'n':
-            return False
+        domain_name = input("Enter the domain name: ")
+        mx_result = retrieve_mx_records(domain_name)
+        
+        if mx_result[0].startswith("Domain") or mx_result[0].startswith("No MX records") or mx_result[0].startswith("Error"):
+            for error_message in mx_result:
+                print(error_message)
         else:
-            print("Invalid choice. Please enter 'y' for yes or 'n' for no.")
-
-if ask_to_restart():
-    pass
-else:
-    print("Program closed.")
+            print("MX records for {} are:".format(domain_name))
+            for mx_server in mx_result:
+                print("- {}".format(mx_server))
+        
+        choice = input("Do you want to start again? (y/n): ").lower()
+        if choice != 'y':
+            print("Program closed.")
+            break
