c9b98c3304b92e693947ed3ad205f1aeb49920db
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-14 09:05:18 +0200

    Update reverse_dns.py

--- Src/Tools/reverse_dns.py
+++ Src/Tools/reverse_dns.py
@@ -4,17 +4,8 @@ def reverse_dns_lookup(ip_address):
     try:
         host_name = socket.gethostbyaddr(ip_address)
         return host_name[0]
-    except socket.herror:
-        return "Reverse DNS lookup failed for {}".format(ip_address)
-
-if __name__ == "__main__":
-    ip_address = input("Enter an IP address for reverse DNS lookup: ")
-    result = reverse_dns_lookup(ip_address)
-    
-    if result.startswith("Reverse DNS lookup failed"):
-        print(result)
-    else:
-        print("Reverse DNS lookup for {} is: {}".format(ip_address, result))
+    except socket.herror as e:
+        return "Reverse DNS lookup failed: {}".format(e)
 
 def ask_to_restart():
     while True:
@@ -26,7 +17,16 @@ def ask_to_restart():
         else:
             print("Invalid choice. Please enter 'y' for yes or 'n' for no.")
 
-if ask_to_restart():
-    pass
-else:
-    print("Program closed.")
+if __name__ == "__main__":
+    while True:
+        ip_address = input("Enter an IP address for reverse DNS lookup: ")
+        result = reverse_dns_lookup(ip_address)
+
+        if result.startswith("Reverse DNS lookup failed"):
+            print(result)
+        else:
+            print("Reverse DNS lookup for {} is: {}".format(ip_address, result))
+
+        if not ask_to_restart():
+            print("Program closed.")
+            break
