23f3efdbaf58c63aabe3bed4c5400c0f729e27ae
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-14 09:24:24 +0200

    Update geolocation.py

--- Src/Tools/geolocation.py
+++ Src/Tools/geolocation.py
@@ -16,25 +16,16 @@ def geolocate_ip_address(ip_address):
         return f"Geolocation lookup failed: {e}"
 
 if __name__ == "__main__":
-    ip_address = input("Enter an IP address for geolocation: ")
-    result = geolocate_ip_address(ip_address)
-    
-    if "failed" in result.lower():
-        print(result)
-    else:
-        print("Geolocation for {} is: {}".format(ip_address, result))
-
-def ask_to_restart():
     while True:
-        choice = input("Do you want to start again? (y/n): ").lower()
-        if choice == 'y':
-            return True
-        elif choice == 'n':
-            return False
+        ip_address = input("Enter an IP address for geolocation: ")
+        result = geolocate_ip_address(ip_address)
+        
+        if "failed" in result.lower():
+            print(result)
         else:
-            print("Invalid choice. Please enter 'y' for yes or 'n' for no.")
-
-if ask_to_restart():
-    pass
-else:
-    print("Program closed.")
+            print("Geolocation for {} is: {}".format(ip_address, result))
+        
+        choice = input("Do you want to start again? (y/n): ").lower()
+        if choice != 'y':
+            print("Program closed.")
+            break
