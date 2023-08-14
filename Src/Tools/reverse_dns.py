54ee9201522b92675779e4bcaa4142823d6f622d
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-14 09:27:34 +0200

    Update reverse_dns.py

--- Src/Tools/reverse_dns.py
+++ Src/Tools/reverse_dns.py
@@ -7,15 +7,6 @@ def reverse_dns_lookup(ip_address):
     except socket.herror:
         return "Reverse DNS lookup failed for {}".format(ip_address)
 
-if __name__ == "__main__":
-    ip_address = input("Enter an IP address for reverse DNS lookup: ")
-    result = reverse_dns_lookup(ip_address)
-    
-    if "failed" in result.lower():
-        print(result)
-    else:
-        print("Reverse DNS lookup for {} is: {}".format(ip_address, result))
-
 def ask_to_restart():
     while True:
         choice = input("Do you want to start again? (y/n): ").lower()
@@ -26,7 +17,16 @@ def ask_to_restart():
         else:
             print("Invalid choice. Please enter 'y' for yes or 'n' for no.")
 
-if ask_to_restart():
-    pass
-else:
-    print("Program closed.")
+if __name__ == "__main__":
+    while True:
+        ip_address = input("Enter an IP address for reverse DNS lookup: ")
+        result = reverse_dns_lookup(ip_address)
+        
+        if "failed" in result.lower():
+            print(result)
+        else:
+            print("Reverse DNS lookup for {} is: {}".format(ip_address, result))
+        
+        if not ask_to_restart():
+            print("Program closed.")
+            break
