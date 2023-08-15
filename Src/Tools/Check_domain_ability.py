2623fcce96a33a94300433730234287cebb5ed7c
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-15 08:27:02 +0200

    Update Check_domain_ability.py

--- Src/Tools/Check_domain_ability.py
+++ Src/Tools/Check_domain_ability.py
@@ -10,13 +10,26 @@ def check_domain_availability(domain):
     except whois.parser.PywhoisError:
         return "Error checking availability for {}.".format(domain)
 
-if __name__ == "__main__":
+def ask_to_restart():
     while True:
-        domain_name = input("Enter the domain name: ")
+        choice = input("Do you want to start again? (y/n): ").lower()
+        if choice == 'y':
+            return True
+        elif choice == 'n':
+            return False
+        else:
+            print("Invalid choice. Please enter 'y' for yes or 'n' for no.")
+
+if __name__ == "__main__":
+    print("Welcome to the Domain Availability Checker!")
+    print("This tool allows you to check if a domain is available or not.")
+    
+    while True:
+        domain_name = input("\nEnter the domain name: ")
         result = check_domain_availability(domain_name)
         print(result)
         
-        choice = input("Do you want to start again? (y/n): ").lower()
+        choice = input("\nDo you want to check another domain? (y/n): ").lower()
         if choice != 'y':
-            print("Program closed.")
+            print("Program closed. Thank you for using the Domain Availability Checker!")
             break
