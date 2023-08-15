595b78ef4664c25f8395ea90825aff5ac5e140b5
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-15 20:41:36 +0200

    Support for ipv6

--- Src/Tools/Ip_addresses.py
+++ Src/Tools/Ip_addresses.py
@@ -1,28 +1,39 @@
 import socket
 
-def get_ip_address(domain):
+def get_ip_addresses(domain):
     try:
-        ip_address = socket.gethostbyname(domain)
-        return ip_address
-    except socket.gaierror:
-        return "DNS lookup failed for {}".format(domain)
-
-def ask_to_continue():
-    choice = input("Do you want to get the IP address of another domain? (y/n): ").lower()
-    return choice == 'y'
+        ipv4_addresses = []
+        ipv6_addresses = []
+        
+        # Retrieve IPv4 addresses
+        ipv4_info = socket.getaddrinfo(domain, None, socket.AF_INET)
+        ipv4_addresses = [ip[4][0] for ip in ipv4_info]
+        
+        # Retrieve IPv6 addresses
+        ipv6_info = socket.getaddrinfo(domain, None, socket.AF_INET6)
+        ipv6_addresses = [ip[4][0] for ip in ipv6_info]
+        
+        return ipv4_addresses, ipv6_addresses
+    except socket.gaierror as e:
+        return [], []
 
 if __name__ == "__main__":
     print("Welcome to the IP Address Retrieval Tool!")
 
     while True:
         domain_name = input("\nEnter the domain name: ")
-        ip_address = get_ip_address(domain_name)
+        ipv4_addresses, ipv6_addresses = get_ip_addresses(domain_name)
         
-        if ip_address.startswith("DNS lookup failed"):
-            print(ip_address)
+        if not ipv4_addresses and not ipv6_addresses:
+            print(f"No IP addresses found for {domain_name}.")
         else:
-            print("IP address of {} is: {}".format(domain_name, ip_address))
+            print(f"IP addresses of {domain_name} are:")
+            for ipv4 in ipv4_addresses:
+                print(f"- {ipv4} (IPv4)")
+            for ipv6 in ipv6_addresses:
+                print(f"- {ipv6} (IPv6)")
         
-        if not ask_to_continue():
+        choice = input("\nDo you want to get the IP addresses of another domain? (y/n): ").lower()
+        if choice != 'y':
             print("Program closed. Thank you for using the IP Address Retrieval Tool!")
             break
