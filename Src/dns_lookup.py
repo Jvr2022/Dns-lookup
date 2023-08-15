b28a468fdeeddccf36c1171c7f027b2816cdc025
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-15 08:49:26 +0200

    Update dns_lookup.py

--- Src/dns_lookup.py
+++ Src/dns_lookup.py
@@ -53,7 +53,7 @@ def reverse_dns_lookup(ip_address):
         return hostnames[0]
     except socket.herror as e:
         return "Reverse DNS lookup failed for {}: {}".format(ip_address, e)
-
+        
 def check_domain_availability(domain):
     try:
         w = whois.whois(domain)
@@ -76,7 +76,7 @@ def check_dns_propagation_status(domain):
             return "DNS propagation for {} is not complete yet.".format(domain)
     except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer) as e:
         return "DNS propagation status check failed for {}: {}".format(domain, e)
-
+        
 def check_dnssec_validation(domain):
     try:
         resolver = dns.resolver.Resolver(configure=False)
@@ -89,50 +89,46 @@ def check_dnssec_validation(domain):
             return "DNSSEC validation for {} is not enabled.".format(domain)
     except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer) as e:
         return "DNSSEC validation status check failed for {}: {}".format(domain, e)
-
-def export_to_file(filename, content):
-    try:
-        with open(filename, "w") as file:
-            file.write(content)
-        return "Exported to file: {}".format(filename)
-    except Exception as e:
-        return "Error exporting to file {}: {}".format(filename, e)
+        
 
 if __name__ == "__main__":
-    domain_name = input("Enter the domain name: ")
+    print("Welcome to the DNS Lookup Tool!")
     
-    use_custom_dns = input("Do you want to use custom DNS servers? (y/n): ").lower()
-    if use_custom_dns == 'y':
-        custom_dns_servers = input("Enter custom DNS servers (comma-separated): ")
-        custom_dns_list = custom_dns_servers.split(',')
-        resolver = dns.resolver.Resolver(configure=False)
-        resolver.nameservers = custom_dns_list
-    else:
-        resolver = dns.resolver.Resolver(configure=False)
-        resolver.nameservers = ['8.8.8.8', '8.8.4.4']  # Default DNS servers
-    
-    availability_result = check_domain_availability(domain_name)
-    dns_propagation_result = check_dns_propagation_status(domain_name)
-    dnssec_result = check_dnssec_validation(domain_name)
-    
-    ip_result = view_ip_addresses(domain_name)
-    mx_result = retrieve_mx_records(domain_name)
-    
-    if ip_result[0].startswith("DNS lookup failed"):
-        ip_output = ip_result[0]
-    else:
-        ip_output = "\nIP addresses for {} are:\n- {}".format(domain_name, "\n- ".join(ip_result))
-    
-    if mx_result[0].startswith("No MX records"):
-        mx_output = mx_result[0]
-    else:
-        mx_output = "\nMX records for {} are:\n- {}".format(domain_name, "\n- ".join(mx_result))
-    
-    dns_query_result = fast_dns_query(domain_name)
-    ip_address = dns_query_result
-    reverse_dns_result = reverse_dns_lookup(ip_address)
-    
-    output = f"""
+    while True:
+        domain_name = input("\nEnter the domain name: ")
+        
+        use_custom_dns = input("Do you want to use custom DNS servers? (y/n): ").lower()
+        if use_custom_dns == 'y':
+            custom_dns_servers = input("Enter custom DNS servers (comma-separated): ")
+            custom_dns_list = custom_dns_servers.split(',')
+            resolver = dns.resolver.Resolver(configure=False)
+            resolver.nameservers = custom_dns_list
+        else:
+            resolver = dns.resolver.Resolver(configure=False)
+            resolver.nameservers = ['8.8.8.8', '8.8.4.4']  # Default DNS servers
+        
+        availability_result = check_domain_availability(domain_name)
+        dns_propagation_result = check_dns_propagation_status(domain_name)
+        dnssec_result = check_dnssec_validation(domain_name)
+        
+        ip_result = view_ip_addresses(domain_name)
+        mx_result = retrieve_mx_records(domain_name)
+        
+        if ip_result[0].startswith("DNS lookup failed"):
+            ip_output = ip_result[0]
+        else:
+            ip_output = "\nIP addresses for {} are:\n- {}".format(domain_name, "\n- ".join(ip_result))
+        
+        if mx_result[0].startswith("No MX records"):
+            mx_output = mx_result[0]
+        else:
+            mx_output = "\nMX records for {} are:\n- {}".format(domain_name, "\n- ".join(mx_result))
+        
+        dns_query_result = fast_dns_query(domain_name)
+        ip_address = dns_query_result
+        reverse_dns_result = reverse_dns_lookup(ip_address)
+        
+        output = f"""
 Domain availability check for {domain_name}:
 - {availability_result}
 
@@ -152,26 +148,15 @@ Fast and reliable DNS query for {domain_name} is:
 Reverse DNS lookup for {ip_address} is:
 - {reverse_dns_result}
 """
-    output_filename = f"{domain_name}_query_results.txt"
-    export_to_file(output_filename, output)
-    
-    print("\nQuery results exported to:", output_filename)
-    
-    # Display query results on the screen
-    print("\nQuery Results:")
-    print(output)
-    
-def ask_to_restart():
-    while True:
-        choice = input("Do you want to start again? (y/n): ").lower()
-        if choice == 'y':
-            return True
-        elif choice == 'n':
-            return False
-        else:
-            print("Invalid choice. Please enter 'y' for yes or 'n' for no.")
-
-if ask_to_restart():
-    pass
-else:
-    print("Program closed.")
+        output_filename = f"{domain_name}_query_results.txt"
+        
+        with open(output_filename, "w") as file:
+            file.write(output)
+        print("\nQuery results exported to:", output_filename)
+        
+        print("\nQuery Results:")
+        print(output)
+        
+        if not input("\nDo you want to perform another query? (y/n): ").lower() == 'y':
+            print("Program closed. Thank you for using the DNS Lookup Tool!")
+            break
