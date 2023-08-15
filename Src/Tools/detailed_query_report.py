2a87b03d034a510fb6973931e2855adbccd540c4
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-15 08:34:40 +0200

    Update detailed_query_report.py

--- Src/Tools/detailed_query_report.py
+++ Src/Tools/detailed_query_report.py
@@ -4,13 +4,18 @@ def view_detailed_query_report(domain):
     w = whois.whois(domain)
     return w
 
+def ask_to_continue():
+    choice = input("Do you want to view the detailed query report for another domain? (y/n): ").lower()
+    return choice == 'y'
+
 if __name__ == "__main__":
+    print("Welcome to the Detailed Query Report Viewer!")
+
     while True:
-        domain_name = input("Enter the domain name: ")
+        domain_name = input("\nEnter the domain name: ")
         detailed_report = view_detailed_query_report(domain_name)
         print("\nDetailed Query Report for {}:\n{}".format(domain_name, detailed_report))
         
-        restart_choice = input("Do you want to start again? (y/n): ").lower()
-        if restart_choice != 'y':
-            print("Program closed.")
+        if not ask_to_continue():
+            print("Program closed. Thank you for using the Detailed Query Report Viewer!")
             break
