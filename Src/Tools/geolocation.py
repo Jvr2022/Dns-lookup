7d7a6c32fd98035f5764652011973fa4a3776e15
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-16 14:51:13 +0200

    Advanced IP Geolocation

--- Src/Tools/geolocation.py
+++ Src/Tools/geolocation.py
@@ -6,40 +6,40 @@ def geolocate_ip_address(ip_address):
         response = requests.get(f"http://ip-api.com/json/{ip_address}")
         response.raise_for_status()  # Raise an exception for HTTP errors
         data = response.json()
-        
-        city = data.get("city", "Unknown")
-        region = data.get("regionName", "Unknown")
-        country = data.get("country", "Unknown")
-        
-        geolocation_info = f"{city}, {region}, {country}"
-        return geolocation_info
+
+        if data["status"] == "success":
+            city = data.get("city", "Unknown")
+            region = data.get("regionName", "Unknown")
+            country = data.get("country", "Unknown")
+            isp = data.get("isp", "Unknown")
+            lat = data.get("lat", "Unknown")
+            lon = data.get("lon", "Unknown")
+
+            geolocation_info = f"City: {city}\nRegion: {region}\nCountry: {country}\nISP: {isp}\nLatitude: {lat}\nLongitude: {lon}"
+            return geolocation_info
+        else:
+            return f"Geolocation data not available for {ip_address}."
     except requests.exceptions.RequestException as e:
         return f"Geolocation lookup failed: {e}"
 
 if __name__ == "__main__":
-    print("Welcome to the IP Geolocation Tool!")
+    print("Welcome to the Advanced IP Geolocation Tool!")
 
     while True:
         ip_address = input("\nEnter an IP address for geolocation: ")
 
-        # Check if the input is an IPv6 address
-        is_ipv6 = False
+        # Check if the input is a valid IP address
         try:
-            socket.inet_pton(socket.AF_INET6, ip_address)
-            is_ipv6 = True
-        except socket.error:
-            pass
-
-        if is_ipv6:
-            print("IPv6 addresses are not supported for geolocation.")
-        else:
+            socket.inet_pton(socket.AF_INET, ip_address)
             result = geolocate_ip_address(ip_address)
             
             if "failed" in result.lower():
                 print(result)
             else:
-                print("Geolocation for {} is: {}".format(ip_address, result))
-            
+                print("Geolocation details for {}:\n{}".format(ip_address, result))
+        except socket.error:
+            print("Invalid IP address format.")
+        
         if not input("\nDo you want to geolocate another IP address? (y/n): ").lower() == 'y':
-            print("Program closed. Thank you for using the IP Geolocation Tool!")
+            print("Program closed. Thank you for using the Advanced IP Geolocation Tool!")
             break
