b5e4894816bfe4bc95a139142754947b510dca63
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-08-17 06:48:41 +0200

    User-Friendly Error Handling

--- Src/Tools/Check_domain_ability.py
+++ Src/Tools/Check_domain_ability.py
@@ -1,6 +1,13 @@
 import whois
+import re
+
+def is_valid_domain(domain):
+    return re.match(r"^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z]{2,}$", domain, re.IGNORECASE)
 
 def check_domain_availability(domain):
+    if not is_valid_domain(domain):
+        return "Invalid domain format: Please enter a valid domain name."
+    
     try:
         w = whois.whois(domain)
         if w.status is None:
@@ -9,6 +16,22 @@ def check_domain_availability(domain):
             return "Domain {} is not available.".format(domain)
     except whois.parser.PywhoisError:
         return "Error checking availability for {}.".format(domain)
+    except ConnectionError:
+        return "Connection error: Unable to establish a connection to the WHOIS server."
+    except TimeoutError:
+        return "Timeout error: The connection to the WHOIS server timed out."
+    except whois.whois.ServerNotFoundError:
+        return "Server not found error: Unable to locate the WHOIS server for the domain."
+    except whois.whois.WhoisCommandFailed:
+        return "WHOIS command failed error: The WHOIS command for the domain failed."
+    except whois.whois.WhoisRateLimitError:
+        return "WHOIS rate limit error: The rate limit for WHOIS queries has been exceeded."
+    except whois.whois.WhoisRateLimitExceeded:
+        return "WHOIS rate limit exceeded error: The rate limit for WHOIS queries has been exceeded."
+    except whois.whois.FailedParsingWhoisOutput:
+        return "Failed parsing WHOIS output error: Unable to parse the WHOIS response."
+    except Exception as e:
+        return "An error occurred: {}".format(e)
 
 def ask_to_restart():
     while True:
